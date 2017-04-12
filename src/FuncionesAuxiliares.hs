7415354b9f383822598f744ff48f379da3a685f4
GThibeault <gabriel.eric.thibeault@gmail.com>
2017-04-12 10:07:53 -0300

    Ahora zipMaxWith ya no usa recursion explicita

--- src/FuncionesAuxiliares.hs
+++ src/FuncionesAuxiliares.hs
@@ -1,16 +1,17 @@
 module FuncionesAuxiliares where 
+import Data.Function
+import Data.List
 
 on2 :: (b -> c -> d) -> (a -> b) -> (a -> c) -> a -> d
 on2 op f g x = f x `op` g x
 
 (.|..) :: (c -> d) -> (a -> b -> c) -> a -> b -> d
-(.|..) f g = \x y -> f $ g x y
+(.|..) f g x y = f $ g x y
 
 --se podria hacer todavia mas general con signatura 
 --zipMaxWith' :: (a -> b -> c) -> a -> b -> [a] -> [b] -> [c]
---pero creo que no vale la pena, y asi me salvo de repetir el caso base
+--pero creo que no vale la pena, y asi evitamos repetir el caso base
 zipMaxWith :: (a -> a -> b) -> a -> [a] -> [a] -> [b]
-zipMaxWith f b [] [] = []
-zipMaxWith f b [] (y:ys) = f b y : zipMaxWith f b [] ys
-zipMaxWith f b (x:xs) [] = f x b : zipMaxWith f b xs [] 
-zipMaxWith f b (x:xs) (y:ys) = f x y : zipMaxWith f b xs ys
+zipMaxWith f b xs ys = (zipWith f `on` pad b maxL) xs ys
+                        where maxL = (max `on` length) xs ys
+                              pad b l = on2 (++) id $ unfoldr (\ys -> if length ys >= l then Nothing else Just(b, b:ys))
