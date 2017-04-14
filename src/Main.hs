634d4b5b34b8c9f54d21439d2755b1913a8b741f
gciruelos <gonzalo.ciruelos@gmail.com>
2017-04-14 01:39:26 -0300

    Arreglo tabs raros

--- src/Main.hs
+++ src/Main.hs
@@ -15,9 +15,9 @@ nave5 = Módulo Contenedor nave3 nave2
 nave6 = Módulo Contenedor nave4 nave1
 nave7 = Módulo Contenedor nave1 nave5
 nave8 = Módulo Contenedor nave1 nave6
-nave9 = Módulo Escudo 
-		(Módulo Escudo (Módulo Escudo (Base Escudo) (Base Cañón)) (Módulo Motor (Base Contenedor) (Base Motor))) 
-		(Módulo Escudo (Módulo Contenedor (Base Motor) (Base Contenedor)) (Módulo Escudo (Base Cañón) (Base Escudo)))
+nave9 = Módulo Escudo
+      (Módulo Escudo (Módulo Escudo (Base Escudo) (Base Cañón)) (Módulo Motor (Base Contenedor) (Base Motor)))
+      (Módulo Escudo (Módulo Contenedor (Base Motor) (Base Contenedor)) (Módulo Escudo (Base Cañón) (Base Escudo)))
 
 soloUnMotor = Base Motor
 puroContenedor = Módulo Contenedor (Base Contenedor) (Base Contenedor)
